c2332f37af60094374eb8b394ef7fddc373d9c52
niteen <nitingudale2000@gmail.com>
2024-09-15 17:17:25 +0530

    first commit

---
+++ ForLoop.java
@@ -0,0 +1,41 @@
+package loops;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+	/*	
+		for(int i = 1; i<=10; i++) {
+			
+			System.out.println(i);
+		}*/
+		
+	/*	boolean flag =false;
+		
+		if(flag) {
+			
+			for(int i=0; i<=5; i++) {
+				
+				System.out.println(i);
+			}
+			
+		}
+		else {
+			
+			for(int i =5; i>=1; i--) {
+				
+				System.out.println(i);
+			}
+		}*/
+         for(int i = 1; i<=10; i++) {
+        	
+        	 if(i!=2 && i!=4 && i!=6) 
+        	 {
+        		 System.out.println(i);
+        	 }
+			
+			
+		}
+	}
+
+}
---
+++ WhileLoop.java
@@ -0,0 +1,66 @@
+package loops;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+
+  /* int  i=1;
+     
+     while(i<=10) {
+    	 
+    	 System.out.println(i);
+    	 i++;
+     }*/
+     
+    /* int x = 1;
+     
+     while(x<=10) {
+    	 
+    	 System.out.println("hello "+x);
+    	 x++;
+    	 
+     }*/
+		
+		/*int y = 1;
+		
+		while(y<=10) {
+			
+			System.out.println(y);
+			y+=2;
+		
+		
+		}*/
+	
+    /* int i=2;
+     
+     while(i<=10) {
+    	 
+    	 System.out.println(i);
+    	 i+=2;
+     }*/
+		
+		/*int i=1;
+		
+		while(i<=10) {
+			
+			
+			if(i%2!=0) {
+				
+				System.out.println(i);
+			}
+			
+			i++;
+		}*/
+		
+		int i= 10;
+		
+		while(i>=1) {
+			
+			System.out.println(i);
+			
+			i--;
+		}
+     
+	}
+
+}
